afcdb6788cba1b82cc532be981929aa183742f8f
pineda24 <85385878+pineda24@users.noreply.github.com>
2023-03-20 02:16:37 -0500

    add styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  {
+    path: "", component: MainComponent, children: []
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CleanComponent } from './layout/clean/clean.component';
+import { FooterComponent } from './layout/main/footer/footer.component';
+import { HeaderComponent } from './layout/main/header/header.component';
+import { MenuComponent } from './layout/main/menu/menu.component';
+import { MainComponent } from './layout/main/main.component';
+import { SideComponent } from './layout/main/side/side.component';
+import { MenuVerticalComponent } from './layout/main/menu/menu-vertical/menu-vertical.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CleanComponent,
+    MainComponent,
+    MenuComponent,
+    MenuVerticalComponent,
+    FooterComponent,
+    HeaderComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule,
